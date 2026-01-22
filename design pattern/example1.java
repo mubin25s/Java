6f770d2fe553679b3a7f02d6b355c26684644b5d
Mubin <fathummubin26@gmail.com>
2026-01-22 11:08:13 +0600

    Update backend and frontend files

---
+++ design pattern/example1.java
@@ -0,0 +1,14 @@
+public class example1 {
+    public static void max(int num1, int num2) {
+        if (num1 > num2) {
+            System.out.println("num1 is greater");
+        } else {
+            System.out.println("num2 is greater");
+        }
+    }
+
+    public static void main(String[] args) {
+        int num1 = 10, num2 = 20;
+        max(num1, num2);
+    }
+}
